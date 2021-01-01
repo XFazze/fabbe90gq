7242e1c59397fad744d13f687606c7364063ddc8
xfazze <blixtvinge@gmail.com>
2021-01-01 12:32:21 +0100

    hej

---
+++ root/develop.py
@@ -0,0 +1,18 @@
+from flask import *
+# Initializing flask and sql
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+# Root
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+
+
+
+
+# Run the site
+if __name__ == "__main__":
+    app.run()
