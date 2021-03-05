737cbe637fad51b478538ca19ba43c97f40450f1
xfazze <blixtvinge@gmail.com>
2021-03-05 22:52:32 +0100

    al

--- mainscript.py
+++ mainscript.py
@@ -1,4 +1,8 @@
 from flask import *
+from flask_wtf import FlaskForm
+from wtforms import Form, SelectField, validators, SubmitField, IntegerField
+from wtforms.validators import DataRequired
+from ratio_code.basic_copy import *
 # Initializing flask and sql
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
