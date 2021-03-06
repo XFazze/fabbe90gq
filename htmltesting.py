bd201e6ef3b72a1ece539d565e4267b3bd4b6782
xfazze <blixtvinge@gmail.com>
2021-03-06 13:12:05 +0100

    hej

---
+++ htmltesting.py
@@ -0,0 +1,17 @@
+from flask import *
+from flask_wtf import FlaskForm
+from wtforms import Form, SelectField, validators, SubmitField, IntegerField
+from wtforms.validators import DataRequired
+from ratio_code.basic_copy import *
+# Initializing flask and sql
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+# Root
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# Run the site
+if __name__ == "__main__":
+    app.run()
