8fbd4f48b1cdc1009862a2f31b353bc9cb932287
xfazze <blixtvinge@gmail.com>
2021-05-02 17:42:51 +0200

    hej

--- static/js/pappa.js
+++ static/js/pappa.js
@@ -1,11 +1,38 @@
 window.onload = function(){ 
     document.getElementById("name").onclick = function() {
       document.getElementById("namediv").style.display = "block";
+      document.getElementById("vannerdiv").style.display = "none";
       document.getElementById("loggbokdiv").style.display = "none";
+      document.getElementById("paminnelserdiv").style.display = "none";
+      document.getElementById("Installningardiv").style.display = "none";
       }
+    document.getElementById("vanner").onclick = function() {
+      document.getElementById("namediv").style.display = "none";
+      document.getElementById("vannerdiv").style.display = "block";
+      document.getElementById("loggbokdiv").style.display = "none";
+      document.getElementById("paminnelserdiv").style.display = "none";
+      document.getElementById("Installningardiv").style.display = "none";
+    }
     document.getElementById("loggbok").onclick = function() {
-    document.getElementById("loggbokdiv").style.display = "block";
-    document.getElementById("namediv").style.display = "none";
+      document.getElementById("namediv").style.display = "none";
+      document.getElementById("vannerdiv").style.display = "none";
+      document.getElementById("loggbokdiv").style.display = "block";
+      document.getElementById("paminnelserdiv").style.display = "none";
+      document.getElementById("Installningardiv").style.display = "none";
+    }
+    document.getElementById("paminnelser").onclick = function() {
+      document.getElementById("namediv").style.display = "none";
+      document.getElementById("vannerdiv").style.display = "none";
+      document.getElementById("loggbokdiv").style.display = "none";
+      document.getElementById("paminnelserdiv").style.display = "block";
+      document.getElementById("Installningardiv").style.display = "none";
+    }
+    document.getElementById("installnigar").onclick = function() {
+      document.getElementById("namediv").style.display = "none";
+      document.getElementById("vannerdiv").style.display = "none";
+      document.getElementById("loggbokdiv").style.display = "none";
+      document.getElementById("paminnelserdiv").style.display = "none";
+      document.getElementById("Installningardiv").style.display = "block";
     }
     
     };
