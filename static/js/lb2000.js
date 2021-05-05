111321cd882d5d3f6633de20ac5d3c72731d65b8
xfazze <blixtvinge@gmail.com>
2021-05-05 14:26:58 +0200

    hej

--- static/js/lb2000.js
+++ static/js/lb2000.js
@@ -8,4 +8,12 @@ window.onload = function(){
         document.getElementById("masterydivtext").innerHTML = "Show all";
       }
       }
+      var i;
+      for (i = 0; i < 100; i++) {
+        setTimeout(() => { plswork(); }, 1000);
+      }
     };
+function plswork(){
+  document.getElementById("xx").innerHTML = document.getElementById("xx").textContent + "9";
+
+}
