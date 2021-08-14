1f8c4132137f70e90157431a925889b7e69480d2
xfazze <a@fabbe90.gq>
2021-08-14 23:48:09 +0200

    updated bjornbana help

--- static/js/bjornbanan.js
+++ static/js/bjornbanan.js
@@ -5,6 +5,7 @@ window.onload = function(){
       document.getElementById("admin").style.display = "none";
       document.getElementById("moderating").style.display = "none";
       document.getElementById("voice").style.display = "none";
+      document.getElementById("stats").style.display = "none";
       }
     document.getElementById("randombtn").onclick = function() {
       document.getElementById("info").style.display = "none";
@@ -12,6 +13,7 @@ window.onload = function(){
       document.getElementById("admin").style.display = "none";
       document.getElementById("moderating").style.display = "none";
       document.getElementById("voice").style.display = "none";
+      document.getElementById("stats").style.display = "none";
     }
     document.getElementById("adminbtn").onclick = function() {
       document.getElementById("info").style.display = "none";
@@ -19,6 +21,7 @@ window.onload = function(){
       document.getElementById("admin").style.display = "block";
       document.getElementById("moderating").style.display = "none";
       document.getElementById("voice").style.display = "none";
+      document.getElementById("stats").style.display = "none";
     }
     document.getElementById("moderatingbtn").onclick = function() {
       document.getElementById("info").style.display = "none";
@@ -26,6 +29,7 @@ window.onload = function(){
       document.getElementById("admin").style.display = "none";
       document.getElementById("moderating").style.display = "block";
       document.getElementById("voice").style.display = "none";
+      document.getElementById("stats").style.display = "none";
     }
     document.getElementById("voicebtn").onclick = function() {
       document.getElementById("info").style.display = "none";
@@ -33,5 +37,14 @@ window.onload = function(){
       document.getElementById("admin").style.display = "none";
       document.getElementById("moderating").style.display = "none";
       document.getElementById("voice").style.display = "block";
+      document.getElementById("stats").style.display = "none";
+    }
+    document.getElementById("statsbtn").onclick = function() {
+      document.getElementById("info").style.display = "none";
+      document.getElementById("random").style.display = "none";
+      document.getElementById("admin").style.display = "none";
+      document.getElementById("moderating").style.display = "none";
+      document.getElementById("voice").style.display = "none";
+      document.getElementById("stats").style.display = "block";
     }
 }
