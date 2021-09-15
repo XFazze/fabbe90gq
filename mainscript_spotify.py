7d19d33cab03062b806f157a111e084057c6ef27
xfazze <a@fabbe90.gq>
2021-09-15 12:49:38 +0200

    branch made

--- mainscript_spotify.py
+++ mainscript_spotify.py
@@ -3,7 +3,6 @@ from flask_wtf import FlaskForm
 from wtforms import Form, SelectField, validators, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from ratio_code.basic_copy import *
-from config import *
 # Initializing flask and sql
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
