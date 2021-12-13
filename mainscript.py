35b91dc66dcb1f3c3487012d83ef31dd0d5de74d
xfazze <a@fabbe90.gq>
2021-12-13 22:10:35 +0100

    little fixes

--- mainscript.py
+++ mainscript.py
@@ -8,6 +8,7 @@ from blueprints.factorio import factorio
 from blueprints.lb2000 import lb2000
 from blueprints.user import user
 from blueprints.afknotif import afknotif
+from celery import Celery
 
 # Initializing flask and sql
 app = Flask(__name__,  static_folder='static')
@@ -22,6 +23,25 @@ app.register_blueprint(lb2000, url_prefix='/lb2000')
 app.register_blueprint(user, url_prefix='/user')
 app.register_blueprint(afknotif, url_prefix='/afknotif')
 
+# celery background task for discordbot plots
+#TODO fix image hosting and updating of discord plots
+'''
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+'''
 @app.route("/")
 def index():
     return render_template('index.html')
