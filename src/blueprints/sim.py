6b57e91f5a926d0107130c71f6ef98ea9e0544ab
xfazze <a@fabbe90.gq>
2022-02-09 23:36:09 +0100

    reworking sims

--- src/blueprints/sim.py
+++ src/blueprints/sim.py
@@ -1,22 +1,31 @@
 from flask import *
 from pymongo import MongoClient
 from config import *
-#from Crypto.Util import number
+from Crypto.Util import number
 
 sim = Blueprint('sim', __name__)
+
+
+@sim.route("/")
+def sim_index():
+    return render_template('sims/home.html')
 
 
 @sim.route("/rsa", methods=['GET', 'POST'])
 def rsa():
     return render_template('sims/rsa.html', plow=str(number.getPrime(4))+' ||| ' + str(number.getPrime(5)) + ', ' + str(number.getPrime(5))+' ||| ' + str(number.getPrime(4)),
-                           p128=[str(number.getPrime(32)), str(number.getPrime(32)), str(number.getPrime(32)), str(number.getPrime(32))])
+                           p128=[str(number.getPrime(15)), str(number.getPrime(15)), str(number.getPrime(32)), str(number.getPrime(32))])
 
 # chaos triangle
+
+
 @sim.route("/chaostriangle", methods=['GET', 'POST'])
 def chaostriangle():
     return render_template('sims/chaostriangle.html')
 
 # chaos triangle
+
+
 @sim.route("/chaoscircle", methods=['GET', 'POST'])
 def chaoscircle():
     return render_template('sims/chaoscircle.html')
@@ -34,3 +43,10 @@ def sortingalgorithms():
 @sim.route("/rng", methods=['GET', 'POST'])
 def rng():
     return render_template('sims/rng.html')
+
+# Path finders
+
+
+@sim.route("/pathFinders", methods=['GET', 'POST'])
+def pathFinders():
+    return render_template('sims/pathFinders.html')
