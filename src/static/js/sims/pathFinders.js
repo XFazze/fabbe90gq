fa2a5d45842c91481aed2670e4bb0459b06c61d3
xfazze <a@fabbe90.gq>
2022-02-11 03:11:21 +0100

    started pathfinding

---
+++ src/static/js/sims/pathFinders.js
@@ -0,0 +1,16 @@
+
+window.onload = function () {
+};
+
+// TODO the nodes and edges
+// default
+// preset
+// edited preset
+// PRIORITY TODO VIEW A GRAPH
+
+// TODO select algorithm/purpuse
+// From list
+
+// TODO View solutions 
+// TODO View algorithm in action
+
